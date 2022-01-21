22ad67e9054e09e330c9c582134e6012569da468
Neill Corlett <corlett@google.com>
2022-01-20 21:50:30 -0500

    UPSTREAM: Add mediatek_i2c_spi interface
    Add a spi_master interface supporting MediaTek MST9U ISP mode.
    
    Autodetect the bus type via I2C_FUNC_I2C, and use the appropriate
    read/write commands, in case the MST9U is attached to smbus.
    
    BUG=none
    BRANCH=none
    TEST=Successfully programmed SPI on test hardware.
    
    Original-Change-Id: I24adb14e7b4f7160e1c3ff941774064d5a81e820
    Original-Signed-off-by: Neill Corlett <corlett@google.com>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/61288
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    (cherry picked from commit e98b2d11846fe0f7a64e8739ec5d8e901a54dfc8)
    
    Change-Id: If10d288a908c43abeba31e14832bafc473d12868
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Signed-off-by: Rehan Ghori <rehang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/3432548
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Neill Corlett <corlett@google.com>
    Tested-by: Neill Corlett <corlett@google.com>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

--- programmer.h
+++ programmer.h
@@ -91,6 +91,7 @@ extern const struct programmer_entry programmer_linux_mtd;
 extern const struct programmer_entry programmer_linux_spi;
 extern const struct programmer_entry programmer_lspcon_i2c_spi;
 extern const struct programmer_entry programmer_mec1308;
+extern const struct programmer_entry programmer_mediatek_i2c_spi;
 extern const struct programmer_entry programmer_mstarddc_spi;
 extern const struct programmer_entry programmer_ni845x_spi;
 extern const struct programmer_entry programmer_nic3com;
